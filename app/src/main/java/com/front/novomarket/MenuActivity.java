1c67cb5a3d9323e778353859711a1f468754af6e
unknown <rsarazu22@gmail.com>
2021-05-04 18:21:36 -0500

    Realización del login

---
+++ app/src/main/java/com/front/novomarket/MenuActivity.java
@@ -0,0 +1,17 @@
+package com.front.novomarket;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+        String welcomeText = "Welcome " + getIntent().getStringExtra("username") + "!";
+        TextView tvWelcome = findViewById(R.id.tvWelcome);
+        tvWelcome.setText(welcomeText);
+    }
+}
